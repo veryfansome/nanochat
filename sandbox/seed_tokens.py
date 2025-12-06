a7c9b5d72ed21a52cc10071d06d3da5ff3cf5b45
Fan Zhu <fzhu.admin@icloud.com>
2025-12-06 09:38:05 -0800

    feat(modified-runcpu.sh): quality of life changes for running on CPU

--- sandbox/seed_tokens.py
+++ sandbox/seed_tokens.py
@@ -5,23 +5,25 @@ import yaml
 from nanochat.common import get_base_dir
 
 seeds = set()
-with open(f'{os.path.dirname(os.path.abspath(__file__))}/seed_tokens.yaml', 'r') as f:
-    seed_configs = yaml.safe_load(f)
-    if 'versatile_morphemes' in seed_configs:
-        for seed in seed_configs["versatile_morphemes"]:
-            # Can be beginning, standalone, or inside
-            seeds.add(f" {seed}")
-            seeds.add(f" {seed.capitalize()}")
-            seeds.add(f"{seed}")
-    if 'prefixes' in seed_configs:
-        for seed in seed_configs["prefixes"]:
-            # Always at the beginning
-            seeds.add(f" {seed}")
-            seeds.add(f" {seed.capitalize()}")
-    if 'inner_morphemes' in seed_configs:
-        for seed in seed_configs["inner_morphemes"]:
-            # Always inside or at the end
-            seeds.add(f"{seed}")
+seed_config_file = f"{os.path.dirname(os.path.abspath(__file__))}/seed_tokens.yaml"
+if os.path.exists(seed_config_file):
+    with open(seed_config_file) as f:
+        seed_configs = yaml.safe_load(f)
+        if 'versatile_morphemes' in seed_configs:
+            for seed in seed_configs["versatile_morphemes"]:
+                # Can be beginning, standalone, or inside
+                seeds.add(f" {seed}")
+                seeds.add(f" {seed.capitalize()}")
+                seeds.add(f"{seed}")
+        if 'prefixes' in seed_configs:
+            for seed in seed_configs["prefixes"]:
+                # Always at the beginning
+                seeds.add(f" {seed}")
+                seeds.add(f" {seed.capitalize()}")
+        if 'inner_morphemes' in seed_configs:
+            for seed in seed_configs["inner_morphemes"]:
+                # Always inside or at the end
+                seeds.add(f"{seed}")
 
 with open(f'{get_base_dir()}/seed_tokens.json', 'w') as f:
     seeds = list(seeds)
