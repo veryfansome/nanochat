d2b216d34c9f5b4d5d53596ed9301d2cd24100a8
Fan Zhu <fzhu.admin@icloud.com>
2025-12-16 14:07:45 -0800

    improved sandbox speedrun.sh

--- sandbox/speedrun.sh
+++ sandbox/speedrun.sh
@@ -8,31 +8,20 @@ export NANOCHAT_BASE_DIR="$HOME/.cache/nanochat"
 mkdir -p $NANOCHAT_BASE_DIR
 command -v uv &> /dev/null || curl -LsSf https://astral.sh/uv/install.sh | sh
 [ -d ".venv" ] || uv venv
-uv sync --extra cpu
+
+which nvidia-smi
+NO_CUDA=$?
+
+if ((NO_CUDA)); then
+    uv sync --extra cpu
+else
+    uv sync --extra gpu
+    NPROC_PER_NODE=8
+fi
+
 source .venv/bin/activate
 if [ -z "$WANDB_RUN" ]; then
     WANDB_RUN=dummy
-fi
-
-# The following settings have been adjusted for training a model of similar depth and max_seq_len as speedrun.sh
-# on a MPS machine (M4 Pro, 64GB).
-
-# Requires: TOTAL_BATCH_SIZE % (DEVICE_BATCH_SIZE * MAX_SEQ_LEN * WORLD_SIZE) == 0
-MODEL_DEPTH=${MODEL_DEPTH:-20}
-MAX_SEQ_LEN=${MAX_SEQ_LEN:-2048}
-DEVICE_BATCH_SIZE=${DEVICE_BATCH_SIZE:-2}
-TOTAL_BATCH_SIZE=${total_batch_size:-16384}
-EVAL_TOKENS=${EVAL_TOKENS:-4096}
-CORE_METRIC_MAX_PER_TASK=${CORE_METRIC_MAX_PER_TASK:-12}
-STEPS_PER_RUN=1000
-
-if [ -d "${NANOCHAT_BASE_DIR}/base_checkpoints/d${MODEL_DEPTH}" ]; then
-    # Find last modified checkpoint dir
-    LAST_STEP=$(jq -r .step "$(find "${NANOCHAT_BASE_DIR}/base_checkpoints/d${MODEL_DEPTH}" -type f -name "meta*" -print0 | xargs -0 ls -1t | head -1)")
-    NUM_ITERATIONS=$((LAST_STEP + STEPS_PER_RUN))
-else
-    LAST_STEP=0
-    NUM_ITERATIONS=$STEPS_PER_RUN
 fi
 
 # wipe the report
@@ -50,57 +39,84 @@ if [ ! -f "$NANOCHAT_BASE_DIR/tokenizer/token_bytes.pt" ] || [ ! -f "$NANOCHAT_B
     wait $DATASET_DOWNLOAD_PID
 fi
 
-# train a very small 4 layer model on the CPU
-# each optimization step processes a single sequence of 1024 tokens
-# we only run 50 steps of optimization (bump this to get better results)
+# The following settings have been adjusted for training a model of similar depth and max_seq_len as speedrun.sh
+# on a MPS machine (M4 Pro, 64GB).
+
+MODEL_DEPTH=${MODEL_DEPTH:-20}
+if [ -d "${NANOCHAT_BASE_DIR}/base_checkpoints/d${MODEL_DEPTH}" ]; then
+    # Find last modified checkpoint dir
+    LAST_STEP=$(jq -r .step "$(find "${NANOCHAT_BASE_DIR}/base_checkpoints/d${MODEL_DEPTH}" -type f -name "meta*" -print0 | xargs -0 ls -1t | head -1)")
+else
+    LAST_STEP=0
+fi
+
 BASE_TRAIN_ARGS=(
     --depth="$MODEL_DEPTH"
-    --max_seq_len="$MAX_SEQ_LEN"
-    --device_batch_size="$DEVICE_BATCH_SIZE"
-    --total_batch_size="$TOTAL_BATCH_SIZE"
-    --eval_tokens="$EVAL_TOKENS"
-    --core_metric_max_per_task="$CORE_METRIC_MAX_PER_TASK"
-    --num_iterations="$NUM_ITERATIONS"
+    --run="$WANDB_RUN"
 )
-if [ "$LAST_STEP" -gt 0 ]; then
-  BASE_TRAIN_ARGS+=(--resume_from_step="$LAST_STEP")
+if ((LAST_STEP)); then
+    BASE_TRAIN_ARGS+=(--resume_from_step="$LAST_STEP")
 fi
-python -m scripts.base_train ${BASE_TRAIN_ARGS[*]}
-python -m scripts.base_loss --device_batch_size="$DEVICE_BATCH_SIZE" --split_tokens=4096
-python -m scripts.base_eval --max-per-task=16
+if ((NO_CUDA)); then
+    # Requires: total_batch_size % (device_batch_size * max_seq_len * world_size) == 0
+    # NOTE: world_size = 1 on cpu, 8 on 8XH100
+    BASE_TRAIN_ARGS+=(
+        --device_batch_size="${BASE_TRAIN_DEVICE_BATCH_SIZE:-2}"
+        --total_batch_size="${BASE_TRAIN_TOTAL_BATCH_SIZE:-16384}"
+        --eval_tokens="${BASE_TRAIN_EVAL_TOKENS:-4096}"
+        --core_metric_max_per_task="${BASE_TRAIN_CORE_METRIC_MAX_PER_TASK:-12}"
+        --save_every=5400
+    )
+    python -m scripts.base_train ${BASE_TRAIN_ARGS[*]}
+    python -m scripts.base_loss --device_batch_size="${BASE_LOSS_DEVICE_BATCH_SIZE:-2}" --split_tokens=4096
+    python -m scripts.base_eval --max-per-task=16
+else
+    BASE_TRAIN_ARGS+=(
+        --save_every=1000
+    )
+    torchrun --standalone --nproc_per_node="$NPROC_PER_NODE" -m scripts.base_train -- ${BASE_TRAIN_ARGS[*]}
+    torchrun --standalone --nproc_per_node="$NPROC_PER_NODE" -m scripts.base_loss
+    torchrun --standalone --nproc_per_node="$NPROC_PER_NODE" -m scripts.base_eval
+fi
 
 # midtraining
-if [ "$TRAIN_MID" == 'true' ]; then
+if ! ((NO_CUDA)) || [ "$TRAIN_MID" == 'true' ]; then
     if [ ! -f "$NANOCHAT_BASE_DIR/identity_conversations.jsonl" ]; then
         curl -L -o $NANOCHAT_BASE_DIR/identity_conversations.jsonl https://karpathy-public.s3.us-west-2.amazonaws.com/identity_conversations.jsonl
     fi
-    python -m scripts.mid_train \
-        --max_seq_len="$MAX_SEQ_LEN" \
-        --device_batch_size="$DEVICE_BATCH_SIZE" \
-        --eval_every=50 \
-        --eval_tokens="$EVAL_TOKENS" \
-        --total_batch_size=4096 \
-        --num_iterations=100
-    # eval results will be terrible, this is just to execute the code paths.
-    # note that we lower the execution memory limit to 1MB to avoid warnings on smaller systems
-    python -m scripts.chat_eval --source=mid --max-new-tokens=128 --max-problems=20
+    if ((NO_CUDA)); then
+        python -m scripts.mid_train \
+            --device_batch_size="${MID_TRAIN_DEVICE_BATCH_SIZE:-2}" \
+            --eval_every=50 \
+            --eval_tokens="${MID_TRAIN_EVAL_TOKENS:-4096}" \
+            --total_batch_size=4096 \
+            --num_iterations=100
+        python -m scripts.chat_eval -i=mid --max-new-tokens=128 --max-problems=20
+    else
+        torchrun --standalone --nproc_per_node=$NPROC_PER_NODE -m scripts.mid_train -- --run=$WANDB_RUN
+        torchrun --standalone --nproc_per_node=$NPROC_PER_NODE -m scripts.chat_eval -- -i mid
+    fi
 fi
 
 # SFT
-if [ "$TRAIN_SFT" == 'true' ]; then
-    python -m scripts.chat_sft \
-        --device_batch_size=4 \
-        --target_examples_per_step=32 \
-        --num_iterations=10 \
-        --eval_steps=4 \
-        --eval_metrics_max_problems=16
-    python -m scripts.chat_eval --source=sft --max-new-tokens=128 --max-problems=20
+if ! ((NO_CUDA)) || [ "$TRAIN_SFT" == 'true' ]; then
+    if ((NO_CUDA)); then
+        python -m scripts.chat_sft \
+            --device_batch_size=4 \
+            --target_examples_per_step=32 \
+            --num_iterations=10 \
+            --eval_steps=4 \
+            --eval_metrics_max_problems=16
+        python -m scripts.chat_eval --source=sft --max-new-tokens=128 --max-problems=20
+    else
+        torchrun --standalone --nproc_per_node=$NPROC_PER_NODE -m scripts.chat_sft -- --run=$WANDB_RUN
+        torchrun --standalone --nproc_per_node=$NPROC_PER_NODE -m scripts.chat_eval -- -i sft
+    fi
 fi
 
-# Chat CLI
-# python -m scripts.chat_cli -p "Why is the sky blue?"
-
-# Chat Web
-# python -m scripts.chat_web
+if ! ((NO_CUDA)); then
+    torchrun --standalone --nproc_per_node=$NPROC_PER_NODE -m scripts.chat_rl -- --run=$WANDB_RUN
+    torchrun --standalone --nproc_per_node=$NPROC_PER_NODE -m scripts.chat_eval -- -i rl -a GSM8K
+fi
 
 python -m nanochat.report generate
